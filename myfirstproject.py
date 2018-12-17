e13f28f2cec3b751f5479a66addbabd0bba61044
tomtomcoder155 <45235721+tomtomcoder155@users.noreply.github.com>
2018-12-17 16:03:39 -0700

    This is the  basic game
    Please add to this game, sticking with the theme that Jesus or god is our creator and  is a loving God. Im also open to any other thoughts! Thank you in advance for the help!

---
+++ myfirstproject.py
@@ -0,0 +1,28 @@
+print('You woke me human, What is your name?')
+myName = input('Enter your name here:')
+print(f'You have been a bad species {myName}. I have seen the destruction \nyou cause with your continued pilaging of the earth.\nUnless you can convince me otherwise, prepare to die. So tell me, should I anilihate earth?')
+anilihate = input('Enter Yes or no here:').lower() 
+if anilihate == 'yes':#TODO:Figure out how to stop the script if they answer yes to anilihate
+        print('Very well, (the world starts to rumble, and quickly explodes as nukes hit everywhere)')
+else:
+        print(f'Your answer of {anilihate} perplexes me {myName}. Do you not recognize that your speceies so closely resembles a parasite?')
+parasite = input().lower()
+if parasite == 'yes':
+	print('Then why do you continue to defile this planet?')
+elif parasite == 'no':
+	print('You must be an ignoramis and therefore deserve to die. MY LOGIC IS UNDENIABLE! (A little kitty comes out from under the table with a lazer strapped to its back and commences to vaporize you')
+else:
+	print('Please explain.')
+explination = input().lower()
+print(f"That answer troubles me {myName}. What droves your decisions, what is your moral compass?")
+moralCompass = input().lower()
+if moralCompass == "god" or moralCompass == "jesus":
+	print(f"Tell me about this {moralCompass}. Is it god or a man?")
+god_man = input().lower()
+if god_man == 'god':
+	print('How do you know?')
+if god_man == 'man':
+	print('You are lying! I have seen what man does and it sure isnt done with a moral compass')
+print('LIAR LIAR PANTS ON FIRE, THE WORLD WILL NOW BURN AND YOU WILL EXPIRE (the sound of rockets exploding are the last thing you hear before everything goes black)')
+while god_man <= god_man:
+        god_man = input()
